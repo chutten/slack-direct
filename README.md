71b21ac2bc151231f4a80a816202bfc2c58c43c6
Chris H-C <chutten@mozilla.com>
2019-08-01 14:39:06 -0400

    v0.4 - Fix infinite loop, 'something went wrong' errors

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # slack-direct
 Rewrite links in Slack to be direct and not redirect through slack-redir.net
+
+Find the add-on hosted on AMO: https://addons.mozilla.org/firefox/addon/slack_direct/
--- slack-direct.js
+++ slack-direct.js
@@ -3,10 +3,23 @@ const SLACK_REDIR_PREFIX = "https://slack-redir.net/link?url=";
 // Followed by a suffix starting with '&'
 
 let observer = new MutationObserver(mutationList => {
+  disconnect();
   for (let anchor of document.querySelectorAll(`a[href^="${SLACK_REDIR_PREFIX}"`)) {
+    // Have to manipulate a cloned node as we can't manipulate the `href` successfully directly, for some reason?
     let newAnchor = anchor.cloneNode(true);
+    // Leave the old one around so that Slack's JS doesn't complain
+    anchor.style.display = 'none';
     newAnchor.href = unescape(newAnchor.href.substring(SLACK_REDIR_PREFIX.length, newAnchor.href.indexOf("&")));
-    anchor.parentNode.replaceChild(newAnchor, anchor);
+    anchor.parentNode.insertBefore(newAnchor, anchor);
   }
+  reconnect();
 });
-observer.observe(document.body, {childList: true, subtree: true});
+window.requestIdleCallback(reconnect);
+
+function reconnect() {
+  observer.observe(document.body, {childList: true, subtree: true});
+}
+
+function disconnect() {
+  observer.disconnect();
+}
